cf3f84b72733d711e5e725bf46ef8b0e3c3b9b1b
HmingPuia <hmingpuia2021@gmail.com>
2021-10-21 22:51:21 -0600

    WordText

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './style.css'
+import useWordGame from "./useWordGame";
+
+function App() {
+    const{textBoxRef,handleChange,text, isTimeRunning,timeRemaining,startClock,wordCount}=useWordGame()
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                ref={textBoxRef}
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button onClick={startClock} disabled={isTimeRunning}>Start</button>
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
+}
+
+export default App
